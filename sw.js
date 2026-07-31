afe39b11f1500c3095fc223bebccf1502bb76ad5
Bret Buchanan <bretjb@gmail.com>
2026-07-31 13:45:15 -0500

    feat: PWA manifest and cache-first service worker

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE = 'cc-gm-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/style.css',
+  './js/app.js',
+  './js/dice.js',
+  './js/name-gen.js',
+  './js/npc-gen.js',
+  './js/npc-character-gen.js',
+  './js/initiative.js',
+  './js/dice-roller.js',
+  './js/rules.js',
+  './js/lib/md.js',
+  './data/names.json',
+  './data/npc-components.json',
+  './data/motivations.json',
+  './data/paths.json',
+  './data/gifts-burdens.json',
+  './data/skills.json',
+  './data/abilities.json',
+  './data/archetypes.json',
+  './data/rules/quick-ref.md',
+  './data/rules/full-digest.md',
+  './icon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
